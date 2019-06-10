4a217acead8c4546609a66d13f436a33bec1c094
Andre Ferreira Santos Sousa <afss2@WINDOWS.CIN.UFPE.BR>
2019-06-10 17:15:20 -0300

    ula fazendo

---
+++ cpuVERILOG/ula.v
@@ -0,0 +1,42 @@
+module ula(a,b,tula,outp,stat);
+	input wire [3:0] a;
+	input wire [3:0] b;
+	input wire [2:0] tula;
+	output reg [3:0] outp;
+	output reg stat;
+	
+	always @(*) begin
+	
+		case(tula)
+			3'b000: 
+				outp = a+b;
+			3'b001:
+				outp = a-b;
+			3'b010:
+				outp = ~b+1;
+			3'b011: begin
+				if (a==b)
+					stat = 1;
+				else
+					stat = 0;
+			end
+			3'b100: begin
+				if (a>b)
+					stat = 1'b1;
+				else
+					stat = 1'b0;
+				end
+		
+			3'b101: begin
+				if (a<b)
+					stat=1;
+				else
+					stat=0;
+				end
+			3'b110:
+				outp= a&b;
+			3'b111:
+				outp= a^b;	
+			endcase
+		end
+endmodule
