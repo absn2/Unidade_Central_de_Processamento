8d8496b3ca9abc7926c57bdfea3298f772108cc7
Antonio Barros da Silva Neto <absn2@WINDOWS.CIN.UFPE.BR>
2019-06-27 13:45:41 -0300

    RegistradorX pronto

--- cpuVERILOG/UnidadeDeControle.v
+++ cpuVERILOG/UnidadeDeControle.v
@@ -1,13 +1,14 @@
 module UnidadeDeControle(status,clk,Opcode,tula,Tx,Ty,Tz);
 	
-	input wire clk;
-	input wire [2:00] Opcode;
-	output reg [2:00] Tx,Ty,Tz,tula;
+	input wire clk,status;
+	input wire [2:0] Opcode;
+	output reg [2:0] Tx,Ty,Tz,tula;
 	
-	parameter RESET = 3'b000;
+	parameter HOLD = 3'b000;
 	parameter LOAD = 3'b001;
-	parameter HOLD = 3'b010;
-	parameter SHIFTR = 3'b011;
+	parameter SHIFTR = 3'b010;
+	parameter SHIFTL = 3'b011;
+	parameter RESET = 3'b100;
 	
 	 always @ (posedge clk) begin
 		case (Opcode)
@@ -15,31 +16,31 @@ module UnidadeDeControle(status,clk,Opcode,tula,Tx,Ty,Tz);
 				Ty = RESET;
 				Tx = LOAD;
 				Tz = RESET;
-				tula = RESET;
+				tula = HOLD;
 				end
 			3'b010: begin // ADDLD
 				Ty = LOAD;
 				Tx = LOAD;
 				Tz = HOLD;
-				tula = RESET;
+				tula = HOLD;
 				end
 			3'b011: begin // ADD
-				Ty = HOLD;
+				Ty = LOAD;
 				Tx = HOLD;
 				Tz = HOLD;
-				tula = RESET;
+				tula = HOLD;
 				end
 			3'b100: begin // DIV2
 				Ty = SHIFTR;
 				Tx = HOLD;
 				Tz = HOLD;
-				tula = RESET;
+				tula = HOLD;
 				end
 			3'b101 : begin // DISPLAY 
 				Ty = HOLD;
 				Tx = HOLD;
 				Tz = LOAD;
-				tula = RESET;
+				tula = HOLD;
 				end
 			endcase
 		end
